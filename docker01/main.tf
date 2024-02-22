30f107edcf3562ab07c2a6044b42130a4a6c3861
juan <juanjose1092@gmail.com>
2024-02-22 09:41:39 -0600

    container deployment

--- docker01/main.tf
+++ docker01/main.tf
@@ -21,12 +21,12 @@ resource "proxmox_vm_qemu" "docker01" {
   agent = 1
   clone = "ubuntu-2204-template"
   ciuser = "juan"
-  ipconfig0 = "ip=dhcp"
+  ipconfig0 = "ip=192.168.5.240/24,gw=192.168.5.1"
   qemu_os = "l26"
   full_clone = true
   sockets = 1
-  cores = 2
-  memory = 2048
+  cores = 8
+  memory = 8192
   scsihw = "virtio-scsi-pci"
   sshkeys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDPo139Zepne8Cu/x4fChh11PM+LphQGxdvuz+zW9qMSy9dzdeXG3g0d4AsxAlEHzrE8mi5G9ne4IPFnSmFg+6F6eZCooz1w5b3D3RspZnlM0bdzGrdf8ddrR7wqhDfLWW6qqx3zZWllB+Zo0zRtczjFi+5M7pdt92ktfmeDAthdEa4GL5JyMH1m1NBWXKlkOLt/uDUAqaW5SH64ruLS87bjWUwfMLxRlNSb7eN4dldk9FZm/qKMNPOv6SbMuySRXwcikaPeOPORJdhIWhhjn2NdGqHeWsW2WaTlk/ypcIvq3db66YaktFf51+ijLpVNz/w/8LKh0k4RAoT3a2FBPhR majora"
   cloudinit_cdrom_storage = "primary-datastore"
@@ -42,7 +42,7 @@ resource "proxmox_vm_qemu" "docker01" {
   #/dev/sdb
   disk {
     slot = 1
-    size = "1500G"
+    size = "2000G"
     type = "scsi"
     storage = "primary-datastore"
   }
